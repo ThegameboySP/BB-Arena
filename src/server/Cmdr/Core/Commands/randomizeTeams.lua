70c7c8df94d2c71de52f60598bdecf1f4b80f074
ThegameboySP <delfinocorner@gmail.com>
2022-06-06 17:50:51 -0400

    Add randomizeTeams

---
+++ src/server/Cmdr/Core/Commands/randomizeTeams.lua
@@ -0,0 +1,24 @@
+return {
+	Name = "randomizeTeams";
+	Aliases = {"rt", "sortTeams"};
+	Description = "Randomize a group players between a set of teams.";
+	Group = "Admin";
+	Args = {
+		{
+			Type = "players";
+			Name = "players";
+			Description = "The players to sort into teams.";
+		},
+		{
+			Type = "teams";
+			Name = "teams";
+			Description = "The teams that the players will be sorted into.";
+		},
+		{
+			Type = "boolean";
+			Name = "spawn";
+			Description = "Whether to spawn the players or not.";
+			Optional = true;
+		},
+	}
+}
