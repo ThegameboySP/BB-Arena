5811a2da8c26f7357aaf447c31b117680c4943c6
ThegameboySP <delfinocorner@gmail.com>
2022-06-30 14:16:56 -0400

    Add MapBinder

---
+++ src/shared/Components/MapBinder.lua
@@ -0,0 +1,17 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Common.Component).Component
+return Component:extend("MapBinder", {
+    OnInit = function(self)
+        if self.Instance:IsA("ModuleScript") then
+            local process = require(self.Instance)
+            for k, v in pairs(process) do
+                self[k] = v
+            end
+
+            if process.OnInit then
+                process.OnInit(self)
+            end
+        end
+    end;
+})
