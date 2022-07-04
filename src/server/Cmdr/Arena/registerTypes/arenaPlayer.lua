86488f39f046374bfc261da8ce86a65efb5b0618
ThegameboySP <delfinocorner@gmail.com>
2022-07-04 16:09:25 -0400

    Fix arenaPlayer not working with dup display names

--- src/server/Cmdr/Arena/registerTypes/arenaPlayer.lua
+++ src/server/Cmdr/Arena/registerTypes/arenaPlayer.lua
@@ -1,86 +1,46 @@
 local Players = game:GetService("Players")
 
-local NAME_FORMAT = ("(%s+)@?(%s*)"):format("[^@]", ".")
-
-local function ShorthandSingle (text, executor)
+local function shorthandSingle(text, executor)
 	if text == "me" then
 		return {executor}
-	elseif text == "?" then
-		local players = Players:GetPlayers()
-		return {players[math.random(1, #players)]}
 	end
 end
 
-local function ShorthandMultiple (text, executor)
-	if text == "*" or text == "all" then
+local function shorthandMultiple(text, executor)
+	if text == "all" then
 		return Players:GetPlayers()
 	elseif text == "others" then
-		local Others = Players:GetPlayers()
-		for i = 1, #Others do
-			if Others[i] == executor then
-				table.remove(Others, i)
+		local others = Players:GetPlayers()
+
+		for i = 1, #others do
+			if others[i] == executor then
+				table.remove(others, i)
 				break
 			end
 		end
-		return Others
-	end
 
-	local randomMatch = text:match("%?(%d+)")
-	if randomMatch then
-		local maxSize = tonumber(randomMatch)
-		if maxSize and maxSize > 0 then
-			local players = {}
-			local remainingPlayers = Players:GetPlayers()
-			for i = 1, math.min(maxSize, #remainingPlayers) do
-				table.insert(players, table.remove(remainingPlayers, math.random(1, #remainingPlayers)))
-			end
-
-			return players
-		end
+		return others
 	end
 end
 
-local function CheckShorthands (text, executor, ...)
-	for _, func in pairs({...}) do
+local function checkShorthands(text, executor, ...)
+	for _, func in ipairs({...}) do
 		local values = func(text, executor)
 
-		if values then return values end
+		if values then
+            return values
+        end
 	end
-end
-
-local function getNames(players)
-	local names = {}
-	for _, player in ipairs(players) do
-		local name, displayName = player.Name, player.DisplayName
-		local fullName = displayName
-		if name ~= displayName then
-			fullName ..= "@" .. name
-		end
-
-		table.insert(names, fullName)
-	end
-
-	return names
-end
-
-local function getPlayerByDisplayName(displayName)
-	local lastPlayerFound
-	for _, player in pairs(Players:GetPlayers()) do
-		if player.DisplayName == displayName then
-			if lastPlayerFound then
-				return nil
-			end
-
-			lastPlayerFound = player
-		end
-	end
-
-	return lastPlayerFound
 end
 
 local function getPlayerByName(name)
-	local displayName, name = name:match(NAME_FORMAT)
-	return getPlayerByDisplayName(displayName) or Players:FindFirstChild(name)
+    local firstName, secondName = name:match("^([^@]+)@?(.*)$")
+	
+    if secondName == "" then
+        return Players:FindFirstChild(firstName)
+	else
+    	return Players:FindFirstChild(secondName)
+	end
 end
 
 local function getPlayersByNames(names)
@@ -89,8 +49,9 @@ local function getPlayersByNames(names)
 
 	for _, name in ipairs(names) do
 		local player = getPlayerByName(name)
-		if player == nil then continue end
-		if set[player] then continue end
+		if player == nil or set[player] then
+            continue
+        end
 
 		table.insert(players, player)
 		set[player] = true
@@ -99,65 +60,77 @@ local function getPlayersByNames(names)
 	return players
 end
 
+local function mapNames(players)
+	local names = {}
+
+	for _, player in ipairs(players) do
+        if player.Name == player.DisplayName then
+            table.insert(names, player.DisplayName)
+        else
+            table.insert(names, player.DisplayName .. "@" .. player.Name)
+		end
+	end
+
+	return names
+end
+
 local Util
 local playerType = {
-	Transform = function (text, executor)
-		local shorthand = CheckShorthands(text, executor, ShorthandSingle)
+	Transform = function(text, executor)
+		local shorthand = checkShorthands(text, executor, shorthandSingle)
 		if shorthand then
-			return getNames(shorthand)
+			return mapNames(shorthand)
 		end
 
-		local findName = Util.MakeFuzzyFinder(getNames(Players:GetPlayers()))
+		local findName = Util.MakeFuzzyFinder(mapNames(Players:GetPlayers()))
 
 		return findName(text)
 	end;
 
-	Validate = function (names)
-		if names[1] == nil then
-			return false, "No player with that name could be found."
-		end
-
-		if getPlayerByName(names[1]) == nil then
+	Validate = function(names)
+		if names[1] == nil or getPlayerByName(names[1]) == nil then
 			return false, "No player with that name could be found."
 		end
 
 		return true
 	end;
 
-	Autocomplete = function (names)
-		return getNames(getPlayersByNames(names))
+	Autocomplete = function(names)
+		return mapNames(getPlayersByNames(names))
 	end;
 
-	Parse = function (names)
+	Parse = function(names)
 		return getPlayerByName(names[1])
 	end;
 
-	Default = function (executor)
-		return executor.DisplayName .. "@" .. executor.Name
+	Default = function(executor)
+		return mapNames({executor})[1]
 	end;
 }
 
-return function (registry)
+return function(registry)
 	Util = registry.Cmdr.Util
 
-	local arenaPlayers = Util.MakeListableType(playerType)
-	arenaPlayers.Transform = function(text, executor)
-		local shorthand = CheckShorthands(text, executor, ShorthandSingle, ShorthandMultiple)
+	local playersType = table.clone(playerType)
+	playersType.Listable = true
+
+	playersType.Transform = function(text, executor)
+		local shorthand = checkShorthands(text, executor, shorthandSingle, shorthandMultiple)
 		if shorthand then
-			return getNames(shorthand), true
+			return mapNames(shorthand), true
 		end
 
-		local findNames = Util.MakeFuzzyFinder(getNames(Players:GetPlayers()))
+		local findNames = Util.MakeFuzzyFinder(mapNames(Players:GetPlayers()))
 
 		return findNames(text)
 	end
 
-	arenaPlayers.Parse = function(names, returnAll)
+	playersType.Parse = function(names, returnAll)
 		return returnAll and getPlayersByNames(names) or {getPlayerByName(names[1])}
 	end
 
 	registry:RegisterType("arenaPlayer", playerType)
-	registry:RegisterType("arenaPlayers", arenaPlayers, {
+	registry:RegisterType("arenaPlayers", playersType, {
 		Prefixes = "% teamPlayers";
 	})
 end
