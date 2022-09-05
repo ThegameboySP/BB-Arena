178ab66060af12515e781cebb091b769d0dcbbb9
ThegameboySP <delfinocorner@gmail.com>
2022-09-05 15:23:13 -0400

    Add placeVersion command

---
+++ src/server/Cmdr/Arena/Commands/placeVersion.lua
@@ -0,0 +1,7 @@
+return {
+	Name = "placeVersion";
+	Aliases = {"pv"};
+	Description = "Outputs the current place's version number.";
+	Group = "Any";
+	Args = {};
+}
---
+++ src/server/Cmdr/Arena/Commands/placeVersionServer.lua
@@ -0,0 +1,3 @@
+return function()
+    return game.PlaceVersion
+end
