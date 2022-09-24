34991ee6d13cd00b85983d45a4cbb0352925149c
ThegameboySP <delfinocorner@gmail.com>
2022-09-24 17:06:32 -0400

    Add serverLocation command

---
+++ src/server/Cmdr/Core/Commands/serverLocationServer.lua
@@ -0,0 +1,13 @@
+local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
+
+return function()
+    if RunService:IsStudio() then
+        return "You can't call serverLocation in Studio for privacy reasons"
+    end
+
+    local payload = HttpService:GetAsync("http://ip-api.com/json/")
+    local info = HttpService:JSONDecode(payload)
+
+    return string.format("%s, %s, %s", info.city, info.region, info.country)
+end
