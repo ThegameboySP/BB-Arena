88f18acb4b9df411872f1308493df9c2a3ad8128
ThegameboySP <delfinocorner@gmail.com>
2022-10-10 13:05:46 -0400

    Optimize distant team info

--- src/clientModules/Scripts/teamMembersSeeInfoFarAway.lua
+++ src/clientModules/Scripts/teamMembersSeeInfoFarAway.lua
@@ -1,46 +1,60 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
 local Teams = game:GetService("Teams")
+
+local Signal = require(ReplicatedStorage.Packages.Signal)
+local Effects = require(ReplicatedStorage.Common.Utils.Effects)
 
 local LocalPlayer = Players.LocalPlayer
 
+local function updateHumanoid(humanoid, team)
+    local localTeam = LocalPlayer.Team
+
+    if localTeam == Teams.Spectators or team ~= localTeam then
+        humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Viewer
+        humanoid.HealthDisplayType = Enum.HumanoidHealthDisplayType.DisplayWhenDamaged
+    else
+        humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
+        humanoid.NameDisplayDistance = math.huge
+        humanoid.HealthDisplayType = Enum.HumanoidHealthDisplayType.DisplayWhenDamaged
+        humanoid.HealthDisplayDistance = math.huge
+    end
+end
+
 local function teamMembersSeeInfoFarAway()
-    RunService.Heartbeat:Connect(function()
-        local team = LocalPlayer.Team
-        
-        if team == Teams.Spectators then
-            for _, player in ipairs(Players:GetPlayers()) do
-                if player == LocalPlayer then continue end
-                local char = player.Character
-                if not char then continue end
-                local hum = char:FindFirstChild("Humanoid")
-                if not hum then continue end
-                
-                hum.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Viewer
-                hum.NameOcclusion = Enum.NameOcclusion.EnemyOcclusion
-                hum.HealthDisplayType = Enum.HumanoidHealthDisplayType.DisplayWhenDamaged
+    local localTeamUpdated = Signal.new()
+
+    -- Cache active humanoids and use callbacks for performance.
+    Effects.call(Players, Effects.pipe({
+        Effects.children,
+        function(player, add)
+            if player ~= LocalPlayer then
+                add(player, {player = player})
             end
-        else
-            for _, player in ipairs(Players:GetPlayers()) do
-                if player == LocalPlayer then continue end
-                local char = player.Character
-                if not char then continue end
-                local hum = char:FindFirstChild("Humanoid")
-                if not hum then continue end
-                
-                if player.Team == team then
-                    hum.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
-                    hum.NameOcclusion = Enum.NameOcclusion.NoOcclusion
-                    hum.NameDisplayDistance = math.huge
-                    hum.HealthDisplayType = Enum.HumanoidHealthDisplayType.DisplayWhenDamaged
-                    hum.HealthDisplayDistance = math.huge
-                else
-                    hum.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Viewer
-                    hum.NameOcclusion = Enum.NameOcclusion.EnemyOcclusion
-                    hum.HealthDisplayType = Enum.HumanoidHealthDisplayType.DisplayWhenDamaged
-                end
+        end,
+        Effects.character,
+        function(character, _, _, context)
+            local humanoid = character:FindFirstChild("Humanoid")
+            
+            local connection1 = context.player:GetPropertyChangedSignal("Team"):Connect(function()
+                updateHumanoid(humanoid, context.player.Team)
+            end)
+
+            local connection2 = localTeamUpdated:Connect(function()
+                updateHumanoid(humanoid, context.player.Team)
+            end)
+
+            updateHumanoid(humanoid, context.player.Team)
+
+            return function()
+                connection1:Disconnect()
+                connection2:Disconnect()
             end
         end
+    }))
+
+    LocalPlayer:GetPropertyChangedSignal("Team"):Connect(function()
+        localTeamUpdated:Fire()
     end)
 end
 
